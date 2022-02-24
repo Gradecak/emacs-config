f7ad7e4b8bbd8b3062ce3d6054806c387c5d8f38
Marijan Gradecak <gradecam@tcd.ie>
2022-02-24 11:59:10 +0100

    Update config

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -15,23 +15,11 @@
 (setq default-file-name-handler-alist file-name-handler-alist
       file-name-handler-alist nil)
 
-;; (add-hook
-;;  'server-after-make-frame-hook
-;;  (let (done)
-;;    (lambda ()
-;;      (unless done
-;;        ;; still set done to true even if we hit a bug (otherwise we
-;;        ;; can never open a frame to see the problem)
-;;        (setq done t)
-;;        (initialise-emoji-font)))))
-
 (setq default-frame-alist
         '((width . 80)
           (tool-bar-lines . 0)
-          (font . "FiraCode 13")
+          (font . "FiraCode 11")
           (set-language-environment "UTF-8")))
-
-
 
 (provide 'early-init)
 ;;; early-init.el ends here
