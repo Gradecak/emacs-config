bd420ade12580df2cbef093b0eb8f91811b75923
Marijan Gradecak <maki.gradecak@bloomon.nl>
2022-06-10 10:47:15 +0200

    Revert garbage collector threshold after init

--- early-init.el
+++ early-init.el
@@ -1,8 +1,8 @@
-(defvar default-gc-cons-threshold 16777216 )
-
 ;; make garbage collector less invasive
 (setq gc-cons-threshold  most-positive-fixnum
       gc-cons-percentage 0.6)
+
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
 
 (setq load-prefer-newer t)
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
