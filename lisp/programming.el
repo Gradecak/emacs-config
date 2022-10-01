a017b4b0470afae349707a9035f1f5fbe3abcefe
Marijan Gradecak <maki.gradecak@bloomon.nl>
2022-10-01 14:02:49 +0200

    Move eglot into the programming module

--- lisp/programming.el
+++ lisp/programming.el
@@ -1,5 +1,14 @@
 (require 'use-package)
 
+(use-package eglot
+  :bind
+  (("C-<return> f" . eglot-format-buffer)
+   ("C-<return> S" . eglot-shutdown)
+   ("C-<return> r" . eglot-rename))
+  :config
+  (setq eglot-autoshutdown t
+	eglot-events-buffer-size 0))
+
 (use-package smartparens
     :hook (prog-mode . smartparens-mode)
     :config (require 'smartparens-config))
