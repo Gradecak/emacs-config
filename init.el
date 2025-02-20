30f9fe103c182c19573ddfdc03df3e840b76fdb9
Marijan Gradecak <maki.gradecak@bloomon.nl>
2025-02-20 14:56:01 +0100

    Update straight version

--- init.el
+++ init.el
@@ -1,19 +1,21 @@
 ;; Install straight.el
 
-(setq straight-repository-branch "rr-fix-renamed-variable")
 (setq straight-use-package-by-default t)
 (eval-when-compile
   (defvar bootstrap-version)
   (let ((bootstrap-file
-	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	(bootstrap-version 5))
+         (expand-file-name
+          "straight/repos/straight.el/bootstrap.el"
+          (or (bound-and-true-p straight-base-dir)
+              user-emacs-directory)))
+        (bootstrap-version 7))
     (unless (file-exists-p bootstrap-file)
       (with-current-buffer
           (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
            'silent 'inhibit-cookies)
-	(goto-char (point-max))
-	(eval-print-last-sexp)))
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
     (load bootstrap-file nil 'nomessage))
 
   (straight-use-package 'use-package)
